857d000272e59fc7226381522117781d5ad4d9f7
codelabite <chukwusahpeter@gmail.com>
2021-05-06 14:21:38 +0100

    lofty

---
+++ project/auth_class/lofty-work/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
